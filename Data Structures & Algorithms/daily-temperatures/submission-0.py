50959566afe55f6b801965fbd4c0c5ddc32b0c80
Syed Mohammad Ali <syedmohdali121@gmail.com>
2026-04-02 15:14:44 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        out = [0]*len(temperatures)
+
+        for i in range(len(temperatures)-1):
+            for j in range(i+1, len(temperatures)):
+                if temperatures[j] > temperatures[i]:
+                    out[i]= j-i
+                    break
+        
+        return out
